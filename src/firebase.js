73beca073f7ccfe6d241a683f80194971ef14a60
Tarun <tarundyundi2000@gmail.com>
2021-09-08 03:50:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 export const auth = firebase.initializeApp({
-  apiKey: "AIzaSyDNKJkJXt4JUSiRJnNvKila3Ht6hOC45po",
-  authDomain: "unichat-34ab6.firebaseapp.com",
-  projectId: "unichat-34ab6",
-  storageBucket: "unichat-34ab6.appspot.com",
-  messagingSenderId: "1063085178396",
-  appId: "1:1063085178396:web:6f274d85fc10716074e9d9"
+  apiKey: "API KEY",
+  authDomain: "letschat-34eb6.firebaseapp.com",
+  projectId: "letschat-34eb6",
+  storageBucket: "letschat-34ab6.appspot.com",
+  messagingSenderId: "106305743178396",
+  appId: "1:10630851343496:web:6f274d85fc10744534e9d9"
 }).auth();
