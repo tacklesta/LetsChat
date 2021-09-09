ccacc9cb9387d4a84e7ee68e10e670f6e9f981ef
Tarun <tarundyundi2000@gmail.com>
2021-09-09 15:28:00 +0530

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,22 +1,16 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-
 import { auth } from "../firebase";
 
-// creating a context
 const AuthContext = React.createContext();
 
-// creating a function to grab context
 export const useAuth = ()=> useContext(AuthContext);
 
 
 export const AuthProvider = ({ children }) =>{
-    // setting the user and loading state
     const [loading, setLoading] = useState(true);
     const [user,setUser] = useState(null);
     const history = useHistory();
-
-    // grabbing the user and loading state  from firebase authentication
     useEffect(()=>{
         auth.onAuthStateChanged((user)=>{
             setUser(user);
